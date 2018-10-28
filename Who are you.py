9e0925712ac53972dab30baa39cda695bdcb16a3
Exerriddiam <exerriddiam@hotmail.co.uk>
2018-10-28 18:33:12 +0000

    initial commit

---
+++ Who are you.py
@@ -0,0 +1,77 @@
+import time
+
+time.sleep(3.0)
+
+print ('Hello! I am a new computer programme created by Matt Addicott')
+
+time.sleep(3.0)
+
+print ('\nhe is a clever chap after all')
+
+time.sleep(3.0)
+
+print ('that clever in fact that, if your looking to hire someone.....')
+time.sleep (2.0)
+
+print ('just saying, thats all')
+
+time.sleep (3.0)
+
+print ('anyway :) ')
+
+name = input('Hello new human? whats your name? type in your answer and press enter ')
+
+for i in range(1):
+    print ('\nHello, '+name )
+    
+    time.sleep (1.0)
+    
+    print ('nice to meet you')
+    
+time.sleep(2.0)
+
+age = input('\nmay I ask you to type in your age  ' +name)
+time.sleep(1.0)
+
+for i in range(1):
+    print (age+  ' wow!!!, thats really old! im only a few minutes old!')
+
+time.sleep(4.0)
+
+print ('\nnow that I know your REEEEAAAALLLYYY old')
+
+time.sleep(5.0)
+
+print ('\nI guess you got some real old people hobbies?')
+time.sleep (3.0)
+
+('\n knitting? bowls perhaps?')
+
+time.sleep(6.0)
+
+hobby = input('\nCome on old timer   :/  what is your favourite hobby?  dont forget to press enter')
+time.sleep(3.0)
+
+print ('\nthats quite a good hobby')
+    
+time.sleep(3.0)
+
+print ('\nhave you spelt your hobbie correctly??????')
+    
+time.sleep(4.0)
+
+print ('let me check')
+
+time.sleep(10.0)
+
+print (hobby+ ' hmmmmmmmmm my database is very limited but ill let it pass')
+    
+time.sleep(3.0)
+
+print ('\nnice to meet you ' +name )
+    
+time.sleep(4.0)
+
+print ('\n Thank you for interfacing with me, bye' )
+
+time.sleep(5.0)
